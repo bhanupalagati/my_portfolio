7b557efa68e224b7129f4d89b5eb0e2110e35f6e
BHANU PRAKASH REDDY PALAGATI <bhanu.palagati@gmail.com>
2020-05-25 20:53:15 +0530

    adding low resolution images

--- portfolio/src/app/professional/data.service.ts
+++ portfolio/src/app/professional/data.service.ts
@@ -77,14 +77,18 @@ series: {
           },
           {
             name: "U.G. SEM8",
-            y: 98,
+            y: 100,
+          },
+          {
+            name: "U.G. Aggregate",
+            y: 97.3,
           }
         ]
       }
     ],
   };
   private imgPopup = [
-    {source: 'assets/certificates/coursera/DataAnalysiswithPython.jpg', alt: 'Data Analysis with Python from Coursera'},
+    {source: 'assets/certificates/coursera/ProfessionalDegree.jpg', alt: 'Amalgamation of nine courses with advanced machine learning techniques'},
     {source: 'assets/certificates/coursera/what_is_data_science.jpg', alt: 'What is Data science from Coursera'},
     {source: 'assets/certificates/coursera/ToolsforDataScience.jpg', alt: 'Open Source tools from Coursera'},
     {source: 'assets/certificates/coursera/DataScienceMethodology.jpg', alt: 'Data Science Methodology from Coursera'},
@@ -92,8 +96,8 @@ series: {
     {source: 'assets/certificates/coursera/DatabaseforDataScience.jpg', alt: 'DataBases for Data Science from Coursera'},
     {source: 'assets/certificates/coursera/DataAnalysiswithPython.jpg', alt: 'Data Analysis with Python from Coursera'},
     {source: 'assets/certificates/coursera/VisualisationwithPython.jpg', alt: 'Data Visualization from Coursera'},
-    {source: 'assets/certificates/coursera/DataAnalysiswithPython.jpg', alt: 'Data Analysis with Python from Coursera'},
-    {source: 'assets/certificates/coursera/DataAnalysiswithPython.jpg', alt: 'Data Analysis with Python from Coursera'},
+    {source: 'assets/certificates/coursera/MachineLearningwithPython.jpg', alt: 'Elucidating various ways of using Machine Learning techniques'},
+    {source: 'assets/certificates/coursera/AppliedDataScience.jpg', alt: 'Astounding projects with applied data science'},
     {source: 'assets/certificates/udemy/machineLearning.jpg', alt: 'Machine Learning and R from Udemy'},
     {source: 'assets/certificates/udemy/tableau.jpg', alt: 'Tableau from Udemy'},
     {source: 'assets/certificates/udemy/angular.jpg', alt: 'Angular 8 from Udemy'},
@@ -109,20 +113,30 @@ series: {
   private certificationRows = [
     [
       {id: 0, fheader: 'Coursera', 
-      fcontent: 'IBM Data Science Professional Course', bheader: 'Description', bcontent: ''},
-      {id: 1, fheader: 'Coursera', fcontent: 'What is Data Science', bheader: 'Description', bcontent: ''},
-      {id: 2, fheader: 'Coursera', fcontent: 'Open Source tools for Data Science', bheader: 'Description', bcontent: ''},
-      {id: 3, fheader: 'Coursera', fcontent: 'Data Science Methodology', bheader: 'Description', bcontent: ''},
+      fcontent: 'IBM Data Science Professional Course', bheader: 'Description',
+      bcontent: 'This is an amalgamation of nine courses in coursera. This is provided by IBM and regarded as one of the best professional degree course in coursera. The best part is one has to score more than 80% in order to get the certificate and that will be challenging.'},
+      {id: 1, fheader: 'Coursera', fcontent: 'What is Data Science', bheader: 'Description',
+      bcontent: 'A proper and motivating introduction is vital in order to create interest in the field of study. This course did an stupendous job in that arena by use the 100% real-world examples along with the pros and cons.'},
+      {id: 2, fheader: 'Coursera', fcontent: 'Open Source tools for Data Science', bheader: 'Description',
+      bcontent: 'Anyone can do a job with hard-work. Albeit a person having proper tools to accomplish a job can do an astonishing job in a stipulated amount of time. This course will empower a data science enthusiast with various online working platforms'},
+      {id: 3, fheader: 'Coursera', fcontent: 'Data Science Methodology', bheader: 'Description',
+      bcontent: "One has to question the other in order to make progress in the process of creating a Machine Learning model i.e., appropriate for the customer needs. However, a lot don't know the order of gathering information. This course wil equip the student with the proper model building methodology."},
     ],
     [
-      {id: 4, fheader: 'Coursera', fcontent: 'Python for Data Science and AI', bheader: 'Description', bcontent: ''},
-      {id: 5, fheader: 'Coursera', fcontent: 'Databases and SQL for Data Science', bheader: 'Description', bcontent: ''},
-      {id: 6, fheader: 'Coursera', fcontent: 'Data Analysis with Python', bheader: 'Description', bcontent: ''},
-      {id: 7, fheader: 'Coursera', fcontent: 'Data Visualization with Python', bheader: 'Description', bcontent: ''},
+      {id: 4, fheader: 'Coursera', fcontent: 'Python for Data Science and AI', bheader: 'Description',
+      bcontent: 'Albeit Python is a widely used common programming language, it will be completely different when one creating a statistical model. This course will show the statistical packages and prompt use of those in order to create an efficient yet simple model.'},
+      {id: 5, fheader: 'Coursera', fcontent: 'Databases and SQL for Data Science', bheader: 'Description',
+      bcontent: 'Data science is not more than understanding and using data. Plausibly that data would be in huge amounts, as a result, one needs a database management system to properly manage and facilitate data extraction. This course provides comprehensive knowledge on SQL and cloud platforms supporting it.'},
+      {id: 6, fheader: 'Coursera', fcontent: 'Data Analysis with Python', bheader: 'Description',
+      bcontent: 'This is an extension on the course Python for Data Science and AI. This goes in-depth with various features of Pandas, Numpy, Scipy, Scikit-learn and many more. With this knowledge a model developer can be quick at understanding various algorithms moreover he can write one.'},
+      {id: 7, fheader: 'Coursera', fcontent: 'Data Visualization with Python', bheader: 'Description',
+      bcontent: 'A data scientist can get a lot of insights from numbers. But we cannot expect other people to understand the numbers in the same way. So, we need another way to tell the story to others and it is nothing but Data Visualization. This course equips one with art of telling story using Visualizations.'},
     ],
     [
-      {id: 8, fheader: 'Coursera', fcontent: 'Machine Learning with Python', bheader: 'Description', bcontent: ''},
-      {id: 9, fheader: 'Coursera', fcontent: 'Applied Data Science Capstone', bheader: 'Description', bcontent: ''},
+      {id: 8, fheader: 'Coursera', fcontent: 'Machine Learning with Python', bheader: 'Description',
+      bcontent: 'This course has almost covered every conspicuous algorithm with code and usage. It starts from basics and covers Regression, Classification, Clustering, Recommendation Systems. This course is a project oriented one and includes a project with every algorithm which sums up to 20 of them.'},
+      {id: 9, fheader: 'Coursera', fcontent: 'Applied Data Science Capstone', bheader: 'Description',
+      bcontent: 'This is the capstone project. The main aim of this one is to make the learner to complete a real time level project. Moreover, it also talks about various geographical APIs. In addition, one has to create a model, presentation and a report in order to complete the course. To be honest this is one of the challenging and interesting one.'},
       {id: 10, fheader: 'Udemy', fcontent: 'Machine Learning with R and Python', bheader: 'Description', bcontent: ''},
       {id: 11, fheader: 'Udemy', fcontent: 'Tableau v 20', bheader: 'Description', bcontent: ''},
     ],
